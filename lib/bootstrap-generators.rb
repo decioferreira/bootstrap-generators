2ff9cb37503e34726367d11ebcb52d33449571ed
Décio Ferreira <decio.ferreira@decioferreira.com>
2011-10-07 16:36:24 +0100

    Disabled scaffold css generation

--- lib/bootstrap-generators.rb
+++ lib/bootstrap-generators.rb
@@ -7,6 +7,7 @@ module BootstrapGenerators
       if ::Rails.version.to_f >= 3.1
         # Erubis has the default template engine
         config.app_generators.template_engine :erubis
+        config.app_generators.stylesheets false
         require 'bootstrap-generators/engine'
       else
         # Erubis has the default template engine
@@ -19,4 +20,3 @@ end
 Rails::Generators.hidden_namespaces << 'rails'
 Rails::Generators.hidden_namespaces << 'erb:controller' << 'erb:scaffold'
 Rails::Generators.hidden_namespaces << 'erubis:controller' << 'erubis:scaffold'
-
--- lib/generators/erubis/scaffold/scaffold_generator.rb
+++ lib/generators/erubis/scaffold/scaffold_generator.rb
@@ -22,4 +22,3 @@ module Erubis
     end
   end
 end
-
