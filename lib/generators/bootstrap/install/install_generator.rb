c87370c6aa8616e6a21128b17ff4f67ec0006887
Decio Ferreira <decio.ferreira@decioferreira.com>
2011-10-18 12:24:11 +0100

    haml, haml hero

--- lib/generators/bootstrap/install/install_generator.rb
+++ lib/generators/bootstrap/install/install_generator.rb
@@ -5,15 +5,18 @@ module Bootstrap
     class InstallGenerator < ::Rails::Generators::Base
       desc "Copy BootstrapGenerators default files"
       source_root File.expand_path('../templates', __FILE__)
+      class_option :layout, :desc => "Bootstrap layout templates (hero, fluid or container-app)", :default => "hero", :type => :string
+
+      class_option :template_engine
 
       def copy_lib
-        directory 'lib'
+        directory "lib/templates/#{options[:template_engine]}"
       end
 
       def create_layout
-        template 'application.html.erb', 'app/views/layouts/application.html.erb'
-        copy_file 'bootstrap-generators.css.scss', 'app/assets/stylesheets/bootstrap-generators.css.scss'
-        copy_file 'bootstrap-generators.js', 'app/assets/javascripts/bootstrap-generators.js'
+        template "layouts/#{options[:layout]}.html.#{options[:template_engine]}", "app/views/layouts/application.html.#{options[:template_engine]}"
+        copy_file "layouts/#{options[:layout]}.css.scss", "app/assets/stylesheets/bootstrap-generators.css.scss"
+        copy_file "bootstrap-generators.js", "app/assets/javascripts/bootstrap-generators.js"
       end
     end
   end
--- test/lib/generators/bootstrap/install_generator_test.rb
+++ test/lib/generators/bootstrap/install_generator_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Bootstrap::Generators::InstallGeneratorTest < ::Rails::Generators::TestCase
   destination File.join(Rails.root)
   tests Bootstrap::Generators::InstallGenerator
+  arguments %w(--template-engine erb)
 
   setup :prepare_destination
   setup :copy_routes
@@ -11,23 +12,50 @@ class Bootstrap::Generators::InstallGeneratorTest < ::Rails::Generators::TestCas
     run_generator
 
     assert_file "lib/templates/erb/controller/view.html.erb"
+    assert_no_file "lib/templates/haml/controller/view.html.haml"
   end
 
   test "should copy controller haml templates" do
-    run_generator
+    run_generator %w(--template-engine haml)
 
     assert_file "lib/templates/haml/controller/view.html.haml"
+    assert_no_file "lib/templates/erb/controller/view.html.erb"
   end
 
   test "should copy scaffold erb templates" do
     run_generator
 
-    %w(index edit new show _form).each { |view| assert_file "lib/templates/erb/scaffold/#{view}.html.erb" }
+    %w(index edit new show _form).each { |view|
+      assert_file "lib/templates/erb/scaffold/#{view}.html.erb"
+      assert_no_file "lib/templates/haml/scaffold/#{view}.html.haml"
+    }
   end
 
   test "should copy scaffold haml templates" do
+    run_generator %w(--template-engine haml)
+
+    %w(index edit new show _form).each { |view|
+      assert_file "lib/templates/haml/scaffold/#{view}.html.haml"
+      assert_no_file "lib/templates/erb/scaffold/#{view}.html.erb"
+    }
+  end
+
+  test "should copy css and javascript files" do
     run_generator
 
-    %w(index edit new show _form).each { |view| assert_file "lib/templates/haml/scaffold/#{view}.html.haml" }
+    assert_file "app/assets/stylesheets/bootstrap-generators.css.scss"
+    assert_file "app/assets/javascripts/bootstrap-generators.js"
+  end
+
+  test "should create erb layout" do
+    run_generator
+
+    assert_file "app/views/layouts/application.html.erb"
+  end
+
+  test "should create haml layout" do
+    run_generator %w(--template-engine haml)
+
+    assert_file "app/views/layouts/application.html.haml"
   end
 end
