9d1eff7377e37ca7d36753f7952c7fff76941b7c
Décio Ferreira <decio.ferreira@decioferreira.com>
2011-10-26 17:51:34 +0100

    removed custom code for the form partial

--- lib/bootstrap-generators.rb
+++ lib/bootstrap-generators.rb
@@ -14,33 +14,3 @@ module Bootstrap
   end
 end
 
-# FIXME: Move this from here (find if this is the best way to do it)
-module Rails
-  module Generators
-    class GeneratedAttribute
-      def simple_form_input_class
-        @simple_form_input_class ||= case field_type
-          when :time_select     then 'small'
-          when :datetime_select then 'small'
-          when :date_select     then 'small'
-          when :check_box       then ''
-          else
-            'xxlarge'
-        end
-      end
-    end
-  end
-end
-
-require 'simple_form'
-
-module SimpleForm
-  class ErrorNotification
-    def render
-      if has_errors?
-        template.content_tag(:div, template.content_tag(error_notification_tag, error_message, html_options), :class => 'alert-message error')
-      end
-    end
-  end
-end
-
