56d43d96eaaf9e74eef7cbbdc025da650d2ffb62
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-05-23 13:34:30 +0100

    fix bug with less: variable @baseLineHeight is undefined

--- lib/bootstrap/generators/engine.rb
+++ lib/bootstrap/generators/engine.rb
@@ -1,5 +1,9 @@
 module Bootstrap
   module Generators
-    class Engine < ::Rails::Engine; end
+    class Engine < ::Rails::Engine
+      initializer 'bootstrap-generators.setup', :after => 'less-rails.after.load_config_initializers' do |app|
+        app.config.less.paths << File.join(config.root, 'vendor', 'bootstrap')
+      end
+    end
   end
 end
