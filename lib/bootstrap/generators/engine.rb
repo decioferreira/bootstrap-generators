17c3892b041cf7961ca37f4d294098f2285e0fcb
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-05-23 14:14:13 +0100

    add exception when less is not installed

--- lib/bootstrap/generators/engine.rb
+++ lib/bootstrap/generators/engine.rb
@@ -2,7 +2,11 @@ module Bootstrap
   module Generators
     class Engine < ::Rails::Engine
       initializer 'bootstrap-generators.setup', :after => 'less-rails.after.load_config_initializers' do |app|
-        app.config.less.paths << File.join(config.root, 'vendor', 'bootstrap')
+        begin # check if less is installed
+          app.config.less.paths << File.join(config.root, 'vendor', 'bootstrap')
+        rescue
+          # less not installed
+        end
       end
     end
   end
