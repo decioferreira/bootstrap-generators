94d3d7a0190603d2d0635d8ab25d45ee5b4cf748
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-05-31 12:07:37 +0100

    added sass and less submodules

--- lib/bootstrap/generators/engine.rb
+++ lib/bootstrap/generators/engine.rb
@@ -1,12 +1,9 @@
 module Bootstrap
   module Generators
     class Engine < ::Rails::Engine
-      initializer 'bootstrap-generators.setup', :after => 'less-rails.after.load_config_initializers' do |app|
-        begin # check if less is installed
-          app.config.less.paths << File.join(config.root, 'vendor', 'bootstrap')
-        rescue
-          # less not installed
-        end
+      initializer 'bootstrap-generators.after.load_config_initializers', :after => :load_config_initializers do |app|
+        app.config.less.paths << File.join(config.root, 'vendor', 'twitter', 'bootstrap', 'less') if app.config.less
+        app.config.sass.load_paths << File.join(config.root, 'vendor', 'twitter', 'sass', 'bootstrap', 'lib') if app.config.sass
       end
     end
   end
