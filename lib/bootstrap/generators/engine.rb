d1f1b40f9b2a7f2cb75ae7d8eab024c03680d1fd
Decio Ferreira <decio.jferreira@gmail.com>
2013-04-23 10:58:46 +0100

    Fix assets:precompile

--- lib/bootstrap/generators/engine.rb
+++ lib/bootstrap/generators/engine.rb
@@ -1,9 +1,9 @@
 module Bootstrap
   module Generators
     class Engine < ::Rails::Engine
-      initializer 'bootstrap-generators.after.load_config_initializers', :after => :load_config_initializers do |app|
-        app.config.less.paths << File.join(config.root, 'vendor', 'twitter', 'bootstrap', 'less') if app.config.respond_to?(:less)
-        app.config.sass.load_paths << File.join(config.root, 'vendor', 'twitter', 'bootstrap', 'sass') if app.config.respond_to?(:sass)
+      initializer 'bootstrap-generators.setup', :group => :all do |app|
+        config.less.paths << File.join(config.root, 'vendor', 'twitter', 'bootstrap', 'less') if config.respond_to?(:less)
+        config.sass.load_paths << File.join(config.root, 'vendor', 'twitter', 'bootstrap', 'sass') if config.respond_to?(:sass)
       end
     end
   end
