d9f14ab344805a26eaab402db72f2ed28a559f39
Décio Ferreira <decio.jferreira@gmail.com>
2013-08-23 15:27:09 +0100

    tests passing

--- lib/bootstrap-generators.rb
+++ lib/bootstrap-generators.rb
@@ -4,11 +4,12 @@ module Bootstrap
   module Generators
     class Engine < ::Rails::Engine
       config.app_generators.stylesheets false
-      config.assets.precompile += %w(bootstrap-ie.js)
 
       initializer 'bootstrap-generators.setup' do |app|
         app.config.less.paths << File.expand_path('../../vendor/twitter/bootstrap/less', __FILE__) if app.config.respond_to?(:less)
         app.config.assets.paths << File.expand_path('../../vendor/twitter/bootstrap/sass', __FILE__) if app.config.respond_to?(:sass)
+
+        app.config.assets.precompile += %w(bootstrap-ie.js)
       end
     end
   end
