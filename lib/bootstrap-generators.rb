3558a5b83ec2f498a0d8a8845af5a4882829b2a6
Décio Ferreira <decio.ferreira@decioferreira.com>
2013-10-28 23:31:07 +0000

    add fonts to precompilation

--- lib/bootstrap-generators.rb
+++ lib/bootstrap-generators.rb
@@ -9,6 +9,9 @@ module Bootstrap
         app.config.less.paths << File.expand_path('../../vendor/twitter/bootstrap/less', __FILE__) if app.config.respond_to?(:less)
         app.config.assets.paths << File.expand_path('../../vendor/twitter/bootstrap/sass', __FILE__) if app.config.respond_to?(:sass)
 
+        app.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+        app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
         app.config.assets.precompile += %w(bootstrap-ie.js)
       end
     end
